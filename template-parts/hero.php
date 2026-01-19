3d4aa75ef28677f4429e68a79d89d221a406184f
Aoba Create <logicflow.work@gmail.com>
2026-01-19 18:56:07 +0900

    Add link back to home

--- template-parts/hero.php
+++ template-parts/hero.php
@@ -6,9 +6,15 @@
 
 <header class="hero">
   <h1 class="hero-title">
-    <?php bloginfo( 'name' ); ?>
+    <a href="<?php echo esc_url( home_url('/') ); ?>" class="hero-logo">
+      <img
+        src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/logo.png"
+        alt=""
+      >
+      <span><?php bloginfo( 'name' ); ?></span>
+    </a>
   </h1>
-
+  
   <div class="hero-inner">
     <img
       src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/home/hero4.webp"
